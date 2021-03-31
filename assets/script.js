c71e6af955da7f2ee1b4a85387d0dc63e850463a
ItsEvdok <78257029+ItsEvdok@users.noreply.github.com>
2021-03-31 13:45:12 -0400

    set up a search input

---
+++ assets/script.js
@@ -0,0 +1,35 @@
+var cityName = document.querySelector("#cityInput");
+var pastCitites = document.querySelector("#pastCities");
+var currentCity = document.querySelector("#currentCity");
+var currentTemp = document.querySelector("#currentTemp");
+var currentHum = document.querySelector("#currentHum");
+var currentWind = document.querySelector("#currentWind");
+var currentUv = document.querySelector("#currentUv");
+
+function searchCity(cityInput) {
+    var apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=acf5ece419aae6c97ae296eb5d196fa0";
+
+    var cityInput = cityName.value.trim();
+
+    if(cityInput) {
+        fetch(apiUrl).then(function(response) {
+            response.json().then(function(data){
+                displayInfo(data, cityInput)
+                console.log(data)
+            });
+        });
+    } else {
+        alert('Please enter a valid city');
+    }
+}
+
+function displayInfo() {
+    // displays past cities inputs
+    var newCity = document.createElement("p");
+    newCity.textContent = cityName.value;
+    
+    pastCitites.appendChild(newCity);
+
+    // displays current city info
+    currentCity.textContent = cityName.value;
+}
