32b6c46979d029850ffce05bffb836033e888df8
ItsEvdok <78257029+ItsEvdok@users.noreply.github.com>
2021-04-01 00:29:51 -0400

    small update

--- assets/script.js
+++ assets/script.js
@@ -6,21 +6,41 @@ var currentHum = document.querySelector("#currentHum");
 var currentWind = document.querySelector("#currentWind");
 var currentUv = document.querySelector("#currentUv");
 
+var currentLat = '';
+var currentLon = '';
+
 function searchCity(cityInput) {
     var cityInput = cityName.value.trim();
 
-    var apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&appid=acf5ece419aae6c97ae296eb5d196fa0";
+    var apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&units=imperial&appid=acf5ece419aae6c97ae296eb5d196fa0";
 
     if(cityInput) {
         fetch(apiUrl).then(function(response) {
             response.json().then(function(data){
                 displayInfo(data, cityInput)
-                console.log(data)
+                console.log(data);
+
+                currentLat.textContent = data.coord.lat;
+                currentLon.textContent = data.coord.lon;
             });
         });
     } else {
         alert('Please enter a valid city');
     }
+
+    var currentCityLat = currentLat;
+    var currentCityLon = currentLon;
+
+    var apiUrlTwo = "https://api.openweathermap.org/data/2.5/onecall?lat=" + currentCityLat + "&lon=" + currentCityLon + "&appid=acf5ece419aae6c97ae296eb5d196fa0"
+
+    function displayCity() {
+        fetch(apiUrlTwo).then(function(response){
+            response.json().then(function(data){
+                console.log(data);
+            })
+        })
+    }
+    displayCity();
 }
 
 function displayInfo() {
@@ -29,7 +49,9 @@ function displayInfo() {
     newCity.textContent = cityName.value;
     
     pastCitites.appendChild(newCity);
-
-    // displays current city info
-    currentCity.textContent = cityName.value;
 }
+
+// currentCity.textContent = cityName.value;
+// currentTemp.textContent = 'Temperature: ' + data.main.temp;
+// currentHum.textContent = 'Humidity: ' + data.main.humidity + '%';
+// currentWind.textContent = 'Windspeed: ' + data.wind.speed + 'mph';
