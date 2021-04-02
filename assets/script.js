6867ab4f4491174f3610a19c30fb7dd32bfd8129
ItsEvdok <78257029+ItsEvdok@users.noreply.github.com>
2021-04-02 00:01:48 -0400

    finished display for current day weather info

--- assets/script.js
+++ assets/script.js
@@ -6,10 +6,13 @@ var currentHum = document.querySelector("#currentHum");
 var currentWind = document.querySelector("#currentWind");
 var currentUv = document.querySelector("#currentUv");
 
-var currentLat = '';
-var currentLon = '';
+var currentLat = [];
+var currentLon = [];
 
 function searchCity(cityInput) {
+    currentLat = [];
+    currentLon = [];
+
     var cityInput = cityName.value.trim();
 
     var apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&units=imperial&appid=acf5ece419aae6c97ae296eb5d196fa0";
@@ -19,32 +22,34 @@ function searchCity(cityInput) {
             response.json().then(function(data){
                 displayInfo(data, cityInput)
                 console.log(data);
-                console.log(data.coord[0].value);
+                currentLat.push(data.coord.lat);
+                currentLon.push(data.coord.lon);
 
-                currentLat.textContent = data.coord.lat;
-                currentLon.textContent = data.coord.lon;
+                console.log(currentLat[0]);
+                console.log(currentLon[0]);
 
-                console.log(currentLat);
-                console.log(currentLon);
+                displayCity();
             });
         });
     } else {
         alert('Please enter a valid city');
     }
+}
 
-    var currentCityLat = currentLat;
-    var currentCityLon = currentLon;
+function displayCity() {
+    var apiUrlTwo = "https://api.openweathermap.org/data/2.5/onecall?lat=" + currentLat[0] + "&lon=" + currentLon[0] + "&units=imperial&appid=acf5ece419aae6c97ae296eb5d196fa0"
 
-    var apiUrlTwo = "https://api.openweathermap.org/data/2.5/onecall?lat=" + currentCityLat + "&lon=" + currentCityLon + "&appid=acf5ece419aae6c97ae296eb5d196fa0"
+    fetch(apiUrlTwo).then(function(response){
+        response.json().then(function(data){
+            console.log(data);
 
-    function displayCity() {
-        fetch(apiUrlTwo).then(function(response){
-            response.json().then(function(data){
-                console.log(data);
-            })
+            currentCity.textContent = cityName.value + " " + data.daily[0].dt;
+            currentTemp.textContent = 'Temperature: ' + data.current.temp;
+            currentHum.textContent = 'Humidity: ' + data.current.humidity + '%';
+            currentWind.textContent = 'Windspeed: ' + data.current.wind_speed + 'mph';
+            currentUv.textContent = "UV Index: " + data.current.uvi;
         })
-    }
-    displayCity();
+    })
 }
 
 function displayInfo() {
@@ -55,7 +60,3 @@ function displayInfo() {
     pastCitites.appendChild(newCity);
 }
 
-// currentCity.textContent = cityName.value;
-// currentTemp.textContent = 'Temperature: ' + data.main.temp;
-// currentHum.textContent = 'Humidity: ' + data.main.humidity + '%';
-// currentWind.textContent = 'Windspeed: ' + data.wind.speed + 'mph';
